8908723d180b4cdb973832612bd80bb35b5ea903
“[halil <halilprofil@gmail.com>
2024-10-19 15:38:19 +0300

    card css yazıldı

--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 import Cards from "@/components/cards";
 import SideBar from "@/components/sidebar";
 
 export default function Home() {
   return (
-    <div>
+    <div className="container">
       <SideBar />
-      <Cards />
+      <div className="cardContainer">
+        <Cards />
+      </div>
     </div>
   );
 }
