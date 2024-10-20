af71a4cc7d9dddbd955320644cf2c5be38abb66f
eceylaan <ceylann.ece@gmail.com>
2024-10-20 10:17:32 +0300

    css ekleme

--- app/page.js
+++ app/page.js
@@ -6,15 +6,15 @@ import Header from "@/components/header";
 export default function Home() {
   return (
     <div className="container">
-      <SideBar />
-<<<<<<< HEAD
-      <Header />
-      <Cards />
-=======
+      <div className="sidebar">
+        <SideBar />
+      </div>
+      <div className="header">
+        <Header />
+      </div>
       <div className="cardContainer">
         <Cards />
       </div>
->>>>>>> 8908723d180b4cdb973832612bd80bb35b5ea903
     </div>
   );
 }
